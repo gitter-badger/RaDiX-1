739a54644ab9e4d81bd4b0b7d15cc8e819df7f94
Raul Dipeas <rauldipeas@outlook.com>
2019-03-14 07:05:21 -0300

    / ‘build/build-radix-core.sh’

--- build/build-radix-core.sh
+++ build/build-radix-core.sh
@@ -3,11 +3,13 @@
 echo 'deb https://radix.ws/core-repo cosmic main' > /etc/apt/sources.list.d/rauldipeas.list
 echo 'deb https://master.dl.sourceforge.net/project/radix-core/large-repo cosmic main' >> /etc/apt/sources.list.d/rauldipeas.list
 wget -qO- https://radix.ws/core-repo/rauldipeas.key | apt-key add -
-apt update;apt install -y apt-transport-https build-essential curl libglibmm-2.4-1v5 ruby-dev software-properties-common
+apt update;apt install -y apt-transport-https build-essential curl gdebi-core libglibmm-2.4-1v5 ruby-dev software-properties-common
 # KXStudio
 wget -c https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos_9.5.1~kxstudio3_all.deb
 wget -c https://launchpad.net/~kxstudio-debian/+archive/kxstudio/+files/kxstudio-repos-gcc5_9.5.1~kxstudio3_all.deb
-find / -type f -name "*.deb" -exec dpkg -i {} \;
+gdebi -n kxstudio-repos_9.5.1~kxstudio3_all.deb
+gdebi -n kxstudio-repos-gcc5_9.5.1~kxstudio3_all.deb
+#find / -type f -name "*.deb" -exec dpkg -i {} \;
 apt update;apt dist-upgrade -y;rm -rf kxstudio*
 
 # XanMod (Converter em Deb)
