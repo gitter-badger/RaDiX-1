eabf781c865691d26bc018214c802c5cd97f2751
Raul Dipeas <rauldipeas@outlook.com>
2019-03-20 18:48:33 -0300

    / ‘build/build-iso.md’ / ‘build/build-iso.sh’

--- build/build-iso.sh
+++ build/build-iso.sh
@@ -35,7 +35,7 @@ sudo chroot edit chsh -s /usr/bin/fish root
 sudo chroot edit wget -O oh-my-fish.sh https://get.oh-my.fish
 sudo chroot edit su -c "fish oh-my-fish.sh --noninteractive"
 sudo chroot edit su -c "fish -c 'omf install bobthefish'"
-sudo chroot edit apt install -y --reinstall linux-image-4.18.0-10-generic
+sudo chroot edit DEBIAN_FRONTEND=noninteractive apt install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" linux-generic
 sudo chroot edit update-initramfs -u -k all
 sudo chroot edit sed -i -e 's@user-uid [0-9]*@user-uid 990@' /usr/share/initramfs-tools/scripts/casper-bottom/25adduser
 
