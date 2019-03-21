40d0bf16b116d47c2b702ce8b5b36f32593b3449
Raul Dipeas <rauldipeas@outlook.com>
2019-03-21 14:02:21 -0300

    / ‘build/build-radix-core.sh’

--- build/build-radix-core.sh
+++ build/build-radix-core.sh
@@ -64,7 +64,7 @@ Pin-Priority: 600
 
 Package: fusion-icon
 Pin: origin download.opensuse.org
-Pin-Priority: 600' > /etc/apt/preferences.d/compiz-reloaded.conf
+Pin-Priority: 600' > /etc/apt/preferences.d/compiz-reloaded-pin-600
 # Sublime Text (Converter em Deb)
 wget -qO- https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 echo 'deb https://download.sublimetext.com/ apt/stable/' > /etc/apt/sources.list.d/sublime-text.list
