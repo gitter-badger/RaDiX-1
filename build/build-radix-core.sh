f2d65ceb61e2643b41cf65a9a8ade93a15598ede
Raul Dipeas <rauldipeas@outlook.com>
2019-03-21 10:46:42 -0300

    / ‘build/build-radix-core.sh’

--- build/build-radix-core.sh
+++ build/build-radix-core.sh
@@ -57,6 +57,14 @@ echo 'deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ bionic ma
 # Compiz Reloaded (Converter em Deb)
 wget -qO- http://download.opensuse.org/repositories/home:/stevenpusser:/compiz-reloaded-rebuilds/Debian_Testing/Release.key | apt-key add -
 echo 'deb http://download.opensuse.org/repositories/home:/stevenpusser:/compiz-reloaded-rebuilds/xUbuntu_17.10/ /' > /etc/apt/sources.list.d/compiz-reloaded.list
+echo '
+Package: emerald
+Pin: origin download.opensuse.org
+Pin-Priority: 600
+
+Package: fusion-icon
+Pin: origin download.opensuse.org
+Pin-Priority: 600' > /etc/apt/preferences.d/compiz-reloaded.conf
 # Sublime Text (Converter em Deb)
 wget -qO- https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
 echo 'deb https://download.sublimetext.com/ apt/stable/' > /etc/apt/sources.list.d/sublime-text.list
